e0da10962d88eab8d35627df16a84cef8a80891e
IlyaB <budushkin.ilya@mail.ru>
2025-06-12 00:29:47 +0300

    create special symbols list and write delete function for them

--- checkAI.py
+++ checkAI.py
@@ -1,1 +1,11 @@
-import unicodedata
+zero_symbols = ["\u200b","\u200c","\u200d","\u200e","\u200f"]
+
+def delete_unicode_symbols(text):
+    new_text = "".join([char for char in text if char not in zero_symbols])
+    return repr(new_text)
+
+
+
+
+# a = input()
+# delete_unicode_symbols(a)
